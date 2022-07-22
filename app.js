f30e320860028389095128858fc3aaa5791c2338
Alejandra Rodriguez <jacfalcon.better.practices@gmail.com>
2022-07-22 06:18:23 -0600

    Introduccion a variables y comentarios

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log(console.log('Hola mundo*'));
+let a=10;
+// var b=10; no es buena practiva crear variable con var
+let b = 10, 
+    d =10, 
+    x = a + b;
+const c=10;
+
+//c=20; no se puede hacer
+
+console.log(x);
