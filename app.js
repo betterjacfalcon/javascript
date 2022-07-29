f2cde7c4497adb95c0f797f4bd02bd10843681ec
Alejandra Rodriguez <jacfalcon.better.practices@gmail.com>
2022-07-29 06:34:45 -0600

    Introduccion al manejo de parametros en la console

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 let a=10;
 // var b=10; no es buena practiva crear variable con var
 let b = 10, 
-    d =10, 
+    d =20, 
+    y = 'Hola ',
+    z = 'Alejandra'
     x = a + b;
-const c=10;
+const c=30;
+const saludo = y+z;
 
 //c=20; no se puede hacer
 
-console.log(x);
+console.log('x', x);
+console.warn('x',x);
+console.error('x',x);
+
+console.log('a', a);
+console.log('b', b);
+console.log('c', c);
+
+//Insertan estilos css
+console.log('%c Mis Variables', 'color:blue; font-weight:bold');
+console.log({a});
+console.log({b});
+console.log({c});
+
+console.table({a,b,c,d,x,y,z});
+console.log({saludo});
